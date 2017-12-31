75e34e3cc8461935f26d05e7b711758de2b3dba9
rujoota <rujoota.shah@gmail.com>
2017-12-31 13:04:35 -0800

    Updating after install script

--- scripts/afterinstall.sh
+++ scripts/afterinstall.sh
@@ -4,14 +4,14 @@ chown -R ec2-user:ec2-user .
 chmod -R 755 .
 
 # change user - required for npm install to work correctly
-# su ec2-user<<'EOF'
-echo "user changed here..."
-cd /home/ec2-user/MySite
-echo "dir changed"
-#install application
-npm install
-npm install aws-sdk
-echo "npm install complete"
-#start application
-node server.js
-# EOF
+su ec2-user<<'EOF' 
+    echo "user changed here..."
+    cd /home/ec2-user/MySite
+    echo "dir changed"
+    #install application
+    npm install
+    npm install aws-sdk
+    echo "npm install complete"
+    #start application
+    node server.js
+EOF
